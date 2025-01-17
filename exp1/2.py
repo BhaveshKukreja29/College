79e7db8970df09a75d3dc72cd956b9d83e2a837b
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2025-01-17 19:00:45 +0530

    Create 2.py

---
+++ exp1/2.py
@@ -0,0 +1,4 @@
+name, age = input("Enter name and age seperated by space: ").split()
+#answer = "The name is {} and age is {}".format(name, age)
+#print(answer)
+print("The name is %s and age is %s" % (name, age))
