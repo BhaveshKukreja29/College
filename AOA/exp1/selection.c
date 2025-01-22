3bc41e607ece35ae8502bf605bea9c9c912b9c1b
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2025-01-22 21:25:49 +0530

    Create selection.c

---
+++ AOA/exp1/selection.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main(){
+    int arr[100];
+    int size, i, j, k;
+    
+    int min;
+    int counter = 0, temp = 0, min_ind = 0;
+    printf("Enter array size: ");
+    scanf("%d", &size);
+    
+    for (i = 0; i < size; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Original array: ");
+    
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    do{
+        
+        min = arr[counter];
+        min_ind = counter;
+        
+        for (i = counter; i < size; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+                min_ind = i;
+            }
+        }
+        
+        temp = arr[counter];
+        arr[counter] = arr[min_ind];
+        arr[min_ind] = temp;
+        
+        counter++;
+        
+        printf("For pass %d: ", i);
+        for (k = 0; k < size; k++) {
+            printf("%d ", arr[k]);
+        }
+        printf("\n");
+    }while(counter < size);
+    
+    printf("\nSorted array: ");
+    
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
