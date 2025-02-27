6f0a8ec50e220fea2ef30828589870fcc6305b5e
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2025-02-28 01:47:03 +0530

    Update knap0.c

--- AOA/knap0.c
+++ AOA/knap0.c
@@ -7,8 +7,9 @@ typedef struct Object {
 
 object arr[10];
 
-void knapsack10(int m);
-void print2D(int a[][], int size, int m);
+void knapsack10(int size, int m);
+void print2D(int a[][10], int size, int m);
+void printchar2D(char a[][10], int size, int m);
 
 void main() {
     int size;
@@ -33,38 +34,74 @@ void main() {
     for (int i = 0; i < size; i++) {
         printf("| %-10d | %-10d | %-10d |\n", i + 1, arr[i].profit, arr[i].weight);
     }
+    printf("+------------+------------+------------+\n\n");
     
     printf("Enter the capacity of sack: ");
     scanf("%d", &m);
-
-    printf("+------------+------------+------------+\n");
     
     knapsack10(size, m);
 }
 
 void knapsack10(int size, int m) {
     
-    int DP[size + 1][m + 1] = {0};
-    char keep[size + 1][m + 1] = {'X'};
+    int DP[size + 1][m + 1];
+    char keep[size + 1][m + 1];
     
-    
-    print2D(DP, size, m);
-    print2D(keep, size, m);
-}
-
-void print2D(int a[][], size, m) {
-    for (int i = 0; i < size; i++) {
-        printf("+------------+------------+------------+\n");
-        for (int j = 0; j < m; j++) {
-            printf("| %-4d |", a[i][j]);
+    for (int i = 0; i <= size; i++) {
+        for (int j = 0; j <= m; j++) {
+            DP[i][j] = 0;
         }
-        printf("+------------+------------+------------+\n");
     }
+    
+    for (int i = 0; i <= size; i++) {
+        for (int j = 0; j <= m; j++) {
+            keep[i][j] = '-';
+        }
+    }
+    
+    for (int i = 1; i <= size; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (arr[i - 1].weight <= j) {
+                if (arr[i - 1].profit + DP[i - 1][j - arr[i - 1].weight] > DP[i - 1][j]) {
+                    DP[i][j] = arr[i - 1].profit + DP[i - 1][j - arr[i - 1].weight];
+                    keep[i][j] = '^';
+                }
+                
+                else {
+                    DP[i][j] = DP[i - 1][j];
+                    keep[i][j] = 'x';
+                }
+            }
+            
+            else {
+                DP[i][j] = DP[i - 1][j]; 
+                keep[i][j] = 'x';
+            }
+        }
+        
+       for (int j = 0; j <= m; j++) {
+           printf("Index %d and %d: %d\n", i, j, DP[i][j]);
+       }
+    }
+    
+    printf("\nKnapsack table\n");
+    printf("+---------------------------------------------------------+\n");
+    for (int i = 0; i <= size; i++) {
+        for (int j = 0; j <= m; j++) {
+            printf("| %-2d |", DP[i][j]);
+        }
+        printf("\n");
+    }    
+    printf("+---------------------------------------------------------+\n");
+    
+    printf("\n");
+    printf("\nKeep table\n");
+    printf("+---------------------------------------------------------+\n");
+    for (int i = 0; i <= size; i++) {
+        for (int j = 0; j <= m; j++) {
+            printf("| %-2c |", keep[i][j]);
+        }
+        printf("\n");
+    }
+    printf("+---------------------------------------------------------+\n"); 
 }
-
-
-
-
-
-
-
