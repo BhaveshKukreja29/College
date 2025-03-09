2dc9af62b6b0c8b9695760f7078cb22345b47e92
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2025-03-09 23:53:46 +0530

    Update exp3.c

--- os/exp3.c
+++ os/exp3.c
@@ -8,7 +8,7 @@ typedef struct {
 } process;
 
 process arr[10];
-int output[10];
+process output[10];
 
 int main() {
     printf("Enter number of processes: ");
@@ -46,16 +46,51 @@ int main() {
     // check for more on time after that.
     
     // get first arrival
-    int timespent;
-    int minarrive = 0;
-    int i = 0;
-    while (i < n) {
-        for (int j = 0; j < n; j++) {
-            if (minarrive > arr[i].arrival) {
-                minarrive = arr[i].arrival
+    int timespent = 0;
+    int executed[] = {0,0,0,0,0,0,0,0,0,0};
+    int sizeExecuted = 0;
+    
+    while (sizeExecuted != n) {
+        int i;
+        int minarrive = timespent;
+        int priority = 1000; //took large value to avoid bugs by taking arr.priority[0]
+        
+        process currentProcess;
+        int currentProcessIndex;
+        
+        // figure out the process to select withh respect to arrival and Priority
+        for (i = 0; i < n; i++) {
+            if (minarrive >= arr[i].arrival && priority >= arr[i].priority && executed[i] != 1) {
+                currentProcess = arr[i];
+                priority = currentProcess.priority;
+                currentProcessIndex = i;
             }
         }
-        i++;
+
+        timespent += currentProcess.burst;
+        executed[currentProcessIndex] = 1;
+        
+        for (int j = 0; j < n; j++) {
+            printf(" %d ", executed[j]);
+        }
+        printf("\n");
+        
+        // store process in array for later use
+        output[sizeExecuted] = currentProcess;
+        sizeExecuted++;
+        
     }
+    
+    printf("Output\n\n");
+    
+    printf("\n+----------+----------+----------+----------+\n");
+    printf("|    ID    | Arrival  | Priority |Burst time|\n");
+    printf("+----------+----------+----------+----------+\n");
+    
+    for (int i = 0; i < sizeExecuted; i++) {
+        printf("|    %-6d|    %-6d|    %-6d|    %-6d|\n", output[i].id, output[i].arrival, output[i].priority, output[i].burst);
+        printf("+----------+----------+----------+----------+\n");
+    }
+    
     return 0;
 }
