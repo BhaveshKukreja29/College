04c5e45bbad604f7525888aaf446d4fcd601ea25
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2025-03-10 16:44:14 +0530

    Update exp5.c

--- os/exp5.c
+++ os/exp5.c
@@ -14,3 +14,34 @@
 // after swap we have to keep the new time spent as 0
 
 // after every request, we will increment the time spent of every process
+
+// queue use kar to make O(n)
+
+#include <stdio.h>
+
+typedef struct {
+    char page;
+    int timespent;
+} frame;
+
+int main() {
+    int n;
+    printf("Enter the number of frames: ");
+    scanf("%d", &n);
+    
+    frame frames[n];
+    
+    for (int i = 0; i < n; i++) {
+        frames[i].page = '-';
+        frames[i].timespent = 0;
+    }
+    
+    char req[50];
+    printf("Enter the string of requests: ");
+    scanf("%s", req);
+    
+    printf("%s", req);
+    
+    
+    return 0;
+}
