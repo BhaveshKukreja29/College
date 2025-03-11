bf5d37266de2b5dd5915897a6e6b4d2b71bd89b6
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2025-03-11 15:51:12 +0530

    Create exp82.py

---
+++ python/exp8/exp82.py
@@ -0,0 +1,9 @@
+"""
+Python program to demonstrate use of regular expression 
+
+● Create a phone list using file (surname name number)
+
+Find all the entries of phone book with surname as “Rao” and first name starting with 
+
+‘J’ or ‘K'
+"""
