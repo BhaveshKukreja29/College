248eef4a917999f629bcdece82dff16c6639f1e9
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2025-03-11 15:49:08 +0530

    Create exp81.py

---
+++ python/exp8/exp81.py
@@ -0,0 +1,9 @@
+"""
+Python program to demonstrate use of regular expression 
+
+● Create string with name of cities in india separated by spaces. 
+
+● Find all cities ending with “ai”
+
+● Find all cities starting with “Mu” or “Ma”
+"""
