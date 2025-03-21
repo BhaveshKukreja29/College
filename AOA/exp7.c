cb8aa71e8ae9a6f8bb9f77f2d15cb82ad7bde844
Bhavesh Kukreja <146556002+BhaveshKukreja29@users.noreply.github.com>
2025-03-21 11:23:28 +0530

    Update exp7.c

--- AOA/exp7.c
+++ AOA/exp7.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int D[100][100];
+int D[100][100]; 
+int P[100][100];
 
 void floyd(int v);
 
@@ -9,10 +10,16 @@ void main() {
     printf("Enter number of vertices: ");
     scanf("%d", &v);
     
-    for (int i = 0; i < v; i ++) {
+    for (int i = 0; i < v; i++) {
         for (int j = 0; j < v; j++) {
             printf("Enter weight at edge(%d, %d) (Enter 1000 for infinity): ", i, j);
             scanf("%d", &D[i][j]);
+            
+            if (i != j && D[i][j] != 1000) {
+                P[i][j] = i;  
+            } else {
+                P[i][j] = -1; 
+            }
         }
     }
     
@@ -24,14 +31,25 @@ void floyd(int v) {
         for (int i = 0; i < v; i++) {
             for (int j = 0; j < v; j++) {
                 if (D[i][j] > D[i][k] + D[k][j]) {
-                    D[i][j] = D[i][k] + D[k][j];
+                    D[i][j] = D[i][k] + D[k][j];  
+                    P[i][j] = P[k][j];  
                 }
             }
         }
+        
         printf("K = %d: \n", k);
+        printf("Distance Matrix:\n");
         for (int i = 0; i < v; i++) {
             for (int j = 0; j < v; j++) {
                 printf(" %-4d ", D[i][j]);
+            }
+            printf("\n");
+        }
+
+        printf("Predecessor Matrix:\n");
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                printf(" %-4d ", P[i][j]);
             }
             printf("\n");
         }
